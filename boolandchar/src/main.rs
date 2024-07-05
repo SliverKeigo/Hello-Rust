fdaeaa31261deb172c0b79100f6e5820d05bce6a
SliverKeigo <1226355793@qq.com>
2024-07-06 00:12:45 +0800

    Rust的佈爾值和字符類型

---
+++ boolandchar/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // 使用bool 關鍵字 取值為 true 或 false
+    let checked: bool = true;
+
+    println!(
+        "{}",
+        if checked {
+            "checked is true"
+        } else {
+            "checked is false"
+        }
+    );
+
+    // 字符: 是字符串的基本組成 也就是單個字符或字 UTF-8作為底層編碼,包含數字 字母 Unicode 和 其他特殊字符
+    let c:char = 'R';
+
+    println!("{}", c.len());
+}
--- main.rs
+++ main.rs
@@ -1,3 +1,3 @@
 fn main(){
-  println!("Helllo World!");
+  println!("Hello World!");
 }
